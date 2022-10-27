5ec20868706e813de7a2536ccde80c8a8f5831f8
wachirawit mool-ang <wachirawit050116@gmail.com>
2022-10-27 23:25:55 +0700

    wachirawit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,25 @@
 import React, {Component} from "react";
 import logo from './logo.svg';
 import './App.css';
+import User from "./user/User";
 
-class  App extends Component {
+class App extends Component {
+  state = {
+    name: "React-Chat-Application"
+  }
+
+  onNameChange = name => {
+    this.setState({ name: name });
+  }
+
   render() {
-    return <div className="App"> Hello World!</div>;
+
+    const {name} = this.state
+
+    return <div className="App"> Hello {name}
+    <User name={name} onNameChange={this.onNameChange}/>
+    </div>;
+  
   }
 }
 
